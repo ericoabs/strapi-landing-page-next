d01054a24e4753ddd6e72bdd5bb21f91f5a7314e
Erico Alexandre <ericoabs25@gmail.com>
2021-08-29 10:44:06 -0300

    Configuração do Jest

---
+++ src/styles/render-theme.js
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+
+export const renderTheme = (children) => {
+  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
+};
