80291323e381832a32ee2cb729c6d25b112d7ed0
Erico Alexandre <ericoabs25@gmail.com>
2021-09-07 16:21:42 -0300

    Ajuste de Tipo na Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
-import Home from '../templates/Home';
+import Home, { HomeProps } from '../templates/Home';
 
 import { GetStaticProps } from 'next';
 import { loadPages } from '../api/load-pages';
 
-export type IndexProps = {
-  data: [];
-};
-
-export default function Index({ data = null }: IndexProps) {
+export default function Index({ data = null }: HomeProps) {
   return <Home data={data} />;
 }
 
-export const getStaticProps: GetStaticProps<IndexProps> = async () => {
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   let data = null;
 
   try {
