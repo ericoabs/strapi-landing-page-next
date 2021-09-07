8227be742f7c068b158ac7fe678c37365d897714
Erico Alexandre <ericoabs25@gmail.com>
2021-09-07 17:31:46 -0300

    Migrando o map-data para TypeScript

--- src/api/map-data.ts
+++ src/api/map-data.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { mapSections } from './map-sections';
 import { mapMenu } from './map-menu';
+import { PageData } from '../templates/Home';
 
-export const mapData = (pagesData = [{}]) => {
-  return pagesData.map((data) => {
+export const mapData = (pagesData = [{}] as any): PageData[] => {
+  return pagesData.map((data: any): PageData => {
     const {
       footer_text: footerHtml = '',
       slug = '',
