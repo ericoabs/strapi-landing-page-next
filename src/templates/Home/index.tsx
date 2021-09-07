32aab1b5d7f2ec7ac93fd0f0947495af5f0df855
Erico Alexandre <ericoabs25@gmail.com>
2021-09-07 16:20:29 -0300

    Migrando a Home para Typescript

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,78 @@
+import Head from 'next/head';
+
+import config from '../../config';
+
+import { Base } from '../Base';
+import { GridText, GridTextProps } from '../../components/GridText';
+import { GridImage, GridImageProps } from '../../components/GridImage';
+import { GridContent, GridContentProps } from '../../components/GridContent';
+import {
+  GridTwoColumns,
+  GridTwoColumnsProps,
+} from '../../components/GridTwoColumns';
+import { LogoLinkProps } from '../../components/LogoLink';
+import { MenuLinkProps } from '../../components/MenuLink';
+
+export type PageData = {
+  title: string;
+  slug: string;
+  footerHtml: string;
+  menu: LogoLinkProps & { links: MenuLinkProps[] };
+  sections: SectionProps[];
+};
+
+export type SectionProps = (
+  | GridImageProps
+  | GridTextProps
+  | GridTwoColumnsProps
+  | GridContentProps
+) & { component: string };
+
+export type HomeProps = {
+  data: PageData[];
+};
+
+function Home({ data }: HomeProps) {
+  const { menu, sections, footerHtml, slug, title } = data[0];
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      <Head>
+        <title>
+          {title} | {config.siteName}
+        </title>
+
+        <meta name="description" content="Landing page template" />
+      </Head>
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return (
+            <GridTwoColumns key={key} {...(section as GridTwoColumnsProps)} />
+          );
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...(section as GridContentProps)} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...(section as GridTextProps)} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...(section as GridImageProps)} />;
+        }
+      })}
+    </Base>
+  );
+}
+
+export default Home;
